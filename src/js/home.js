f263bbc2701c66376806984e4e51bc72df67ffa6
Will-h0219 <wherrera0219@gmail.com>
2021-05-24 00:24:22 -0500

    first call to server, cards template

--- src/js/home.js
+++ src/js/home.js
@@ -1,13 +1,45 @@
-class Home{
-    constructor(){
+class Home {
+    constructor() {
         this.listeners();
         this.setFormValidation();
+        this.getPlaces().then(data => {
+            let places = data;
+            let cardsContainer = document.querySelector('.centers-info');
+            let htmltoappend = '';
+            places.forEach((place) => {
+                htmltoappend += this.cardTemplate(place);
+
+            });
+            cardsContainer.innerHTML = htmltoappend;
+        });
     }
 
-    listeners(){
-        let btns = document.querySelectorAll('.submit-btn');
+    async getPlaces() {
+        const response = await fetch("https://localhost:44371/places", { method: 'GET' });
+        return response.json();
+    }
 
-        document.querySelectorAll('.required-text').forEach(function(el){
+    cardTemplate(card) {
+        return `<div class="card">
+                    <img src="${card.image}" class="center-img">
+                    <div class="card-info">
+                        <h4 class="center-name">${card.name}</h4>
+                        <h5 class="subtitle">Dirección</h5>
+                        <p class="sub-info">${card.address}</p>
+                        <h5 class="subtitle">PBX</h5>
+                        <p class="sub-info">${card.pbx}</p>
+                        <p class="sub-info">${card.phone}</p>
+                        <h5 class="subtitle">Horario de atención:</h5>
+                        <p class="sub-info">${card.schedule}</p>
+                    </div>
+                </div>`;
+    }
+
+    listeners() {
+        let btns = document.querySelectorAll('.submit-btn');
+        btns[0].click(checkForm());
+
+        document.querySelectorAll('.required-text').forEach(function (el) {
             el.style.visibility = "hidden";
         });
 
@@ -17,40 +49,40 @@ class Home{
                 let formData = new FormData(form);
                 let sendData = 0;
 
-                for(let pair of formData){
+                for (let pair of formData) {
                     console.log(pair);
-                    if(pair[0]=="affair"){
-                        if(pair[1]=='opt0'){
+                    if (pair[0] == "affair") {
+                        if (pair[1] == 'opt0') {
                             let input = document.querySelector('select[name=affair]');
                             input.parentElement.querySelector('.required-text').innerHTML = 'Opción invalida';
                             input.parentElement.querySelector('.required-text').style.visibility = "visible";
                             alert('Faltan campos por llenar');
                             break;
                         }
-                    }else if(pair[1] == ""){
+                    } else if (pair[1] == "") {
                         let input = document.getElementById(pair[0]);
                         input.parentElement.querySelector('.required-text').style.visibility = "visible";
                         input.focus();
                         alert('Faltan campos por llenar');
                         break;
-                    }else{
+                    } else {
                         sendData = sendData + 1;
                     }
                 }
 
-                if(sendData == 5 && formData.get('affair') != "opt0"){
+                if (sendData == 5 && formData.get('affair') != "opt0") {
                     alert("Información enviada exitosamente!");
                 }
             });
         });
     }
 
-    setFormValidation(){
-        document.querySelector('input[name=username]').addEventListener('keypress', (e)=> {
+    setFormValidation() {
+        document.querySelector('input[name=username]').addEventListener('keypress', (e) => {
             let key = e.key;
             let regexp = /[a-zA-Z0-9 ]/;
-            
-            if(!regexp.exec(key)){
+
+            if (!regexp.exec(key)) {
                 e.preventDefault();
             }
         });
@@ -59,7 +91,7 @@ class Home{
             let key = e.key;
             let numbers = /[0-9]/;
 
-            if(!numbers.exec(key)){
+            if (!numbers.exec(key)) {
                 e.preventDefault();
             }
         });
@@ -68,11 +100,31 @@ class Home{
             let key = e.key;
             let numbers = /[0-9+]/;
 
-            if(!numbers.exec(key)){
+            if (!numbers.exec(key)) {
                 e.preventDefault();
             }
         });
     }
 }
 
+function checkForm() {
+    let btns = document.querySelectorAll('.submit-btn');
+    let form = document.getElementById("contactus-form");
+    let formData = new FormData(form);
+    let checked = true;
+
+    for (let pair of formData) {
+        if (pair[1] == "") {
+            checked = false;
+        }
+    }
+
+    if (checked) {
+        btns[0].disabled = false;
+    } else {
+        btns[0].disabled = true;
+    }
+}
+
 window.addEventListener("load", new Home());
+window.addEventListener("keyup", checkForm);
